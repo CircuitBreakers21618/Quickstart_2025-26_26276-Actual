da9268bc16699a3b8b7441d8ce08f12f8cc715d3
wykzimme <bryce.e.zimmerman@gmail.com>
2025-11-05 17:59:12 -0700

    Update teleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/teleop.java
@@ -10,10 +10,10 @@ public class teleop extends OpMode {
 
     DcMotor lf, rf;
     DcMotor lr, rr;
-    DcMotor intakerel;
+   // DcMotor intakerel;
     Gamepad currentGamepad2 = new Gamepad();
     Gamepad previousGamepad2 = new Gamepad();
-    int intake = 0;;
+    //int intake = 0;;
 
     @Override
     public void init() {
@@ -21,7 +21,7 @@ public class teleop extends OpMode {
         lr = hardwareMap.get(DcMotor.class, "lr");
         rf = hardwareMap.get(DcMotor.class, "rf");
         rr = hardwareMap.get(DcMotor.class, "rr");
-        intakerel = hardwareMap.get(DcMotor.class, "intakerel");
+  //      intakerel = hardwareMap.get(DcMotor.class, "intakerel");
 
 
 
@@ -29,11 +29,11 @@ public class teleop extends OpMode {
 
     @Override
     public void loop() {
-        lf.setPower(gamepad1.left_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
-        lr.setPower(-gamepad1.left_stick_y + -gamepad1.left_stick_x * 1.1+ gamepad1.right_stick_x);
-        rf.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x * 1.1 +gamepad1.right_stick_x);
-        rr.setPower(gamepad1.left_stick_y + -gamepad1.left_stick_x * 1.1 + gamepad1.right_stick_x);
-        intakerel.setPower(gamepad2.right_stick_x);
+        lf.setPower(gamepad1.left_stick_y + -gamepad1.right_stick_x * 1.1 + gamepad1.left_stick_x);
+        lr.setPower(-gamepad1.left_stick_y + -gamepad1.right_stick_x * 1.1+ gamepad1.left_stick_x);
+        rf.setPower(gamepad1.left_stick_y + gamepad1.right_stick_x * 1.1 +gamepad1.left_stick_x);
+        rr.setPower(gamepad1.left_stick_y + -gamepad1.right_stick_x * 1.1 + gamepad1.left_stick_x);
+     /*   intakerel.setPower(gamepad2.right_stick_x);
 
         if (currentGamepad2.a && !previousGamepad2.a) {
             if (intake == 1) { //position 0 -> 1
@@ -53,7 +53,7 @@ public class teleop extends OpMode {
             }
         }
 
-
+*/
 
     }
 }
