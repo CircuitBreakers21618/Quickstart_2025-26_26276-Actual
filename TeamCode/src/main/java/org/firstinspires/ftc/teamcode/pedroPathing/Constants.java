5cd8c90a85576aabb6f6cfc44e8b1fc2e580a619
wykzimme <bryce.e.zimmerman@gmail.com>
2025-12-30 16:24:58 -0700

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,8 +16,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
     .mass(7.2)
-    .forwardZeroPowerAcceleration(-57.43275565148078)
-            .lateralZeroPowerAcceleration(-69.06466312501034)
+    .forwardZeroPowerAcceleration(-43.587112)
+            .lateralZeroPowerAcceleration(-57.22450985)
             .translationalPIDFCoefficients(new PIDFCoefficients(0.3, 0, 0.001, 0.025))
             .headingPIDFCoefficients(new PIDFCoefficients(0.6, 0, 0.005, 0.025))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.01,0.0,0.001,0.6,0.025))
@@ -33,8 +33,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(51.9736);
-           // .yVelocity(0);
+            .xVelocity(51.9736)
+           .yVelocity(44.2946);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
@@ -47,7 +47,7 @@ public class Constants {
     }
     public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
             .forwardTicksToInches(.0020261287634188952)
-            .strafeTicksToInches(.0020538308483531442)
+            .strafeTicksToInches(-.0020538308483531442)
             .turnTicksToInches(.0023361361365676312)
             .leftPodY(8)
             .rightPodY(-8)
